38bca50f40d8f44868235d99cb64a1960dd8639e
unknown <yadavnishu1905@gmail.com>
2024-01-10 12:19:56 +0530

    Added Popular Section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import HeroBanner from "./heroBanner/HeroBanner"
 import "./style.scss"
 import Trending from "./trending/Trending"
+import Popular from "./popular/Popular"
 
 const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner/>
       <Trending/>
+      <Popular/>
       <div style={{height:1000}}></div>
     </div>
   )
